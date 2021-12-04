1e7319dd48505d68c333b886d5abebe638617985
Dustin Welch <dustinwwelch1@gmail.com>
2021-12-03 18:29:26 -0600

    added main landing - still needs work

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // note: make sure to exit server and then re-'yarn start' after importing relative files, like the one above
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-tsparticles";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
